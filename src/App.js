b2d11c1d2431c0440ffd1530a15dbd8d4fa5a1ae
BogdanJas <bogdan.jasinskij@gmail.com>
2024-08-15 16:29:05 +0200

    created simple form example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,217 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+function Button({ onClick, children }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default function App() {
+  const [friends, setFriends] = useState(initialFriends);
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [selectedFriend, setSelectedFriend] = useState(null);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((showAddFriend) => !showAddFriend);
+    setSelectedFriend(null);
+  }
+
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
+  function handleSelectFriend(friend) {
+    setSelectedFriend((selected) =>
+      selected?.id === friend.id ? null : friend
+    );
+    setShowAddFriend(false);
+  }
+
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+    setSelectedFriend(null);
+  }
+
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList
+          friends={friends}
+          selectedFriend={selectedFriend}
+          onClickSelect={handleSelectFriend}
+        />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add friend"}
+        </Button>
+      </div>
+
+      {selectedFriend && (
+        <FormSplitBill friend={selectedFriend} onSplitBill={handleSplitBill} />
+      )}
     </div>
   );
 }
 
-export default App;
+function FriendsList({ friends, onClickSelect, selectedFriend }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          friend={friend}
+          key={friend.id}
+          onClickSelect={onClickSelect}
+          selectedFriend={selectedFriend}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend, onClickSelect, selectedFriend }) {
+  const isSelected = selectedFriend && selectedFriend?.id === friend.id;
+  return (
+    <li className={isSelected ? "selected" : ""}>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 ? (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      ) : friend.balance > 0 ? (
+        <p className="green">
+          {friend.name} owes you {friend.balance}$
+        </p>
+      ) : (
+        <p>You and {friend.name} are even</p>
+      )}
+      <Button onClick={() => onClickSelect(friend)}>
+        {isSelected ? "Close" : "Select"}
+      </Button>
+    </li>
+  );
+}
+
+function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+
+  function handleSubmitAddFriend(e) {
+    e.preventDefault();
+
+    if (name === "") return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id: id,
+      name,
+      image: `https://i.pravatar.cc/?=${id}`,
+      balance: 0,
+    };
+
+    onAddFriend(newFriend);
+
+    setName("");
+  }
+
+  return (
+    <form className="form-add-friend" onSubmit={handleSubmitAddFriend}>
+      <label>👫 Friend name</label>
+      <input
+        type="text"
+        value={name}
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill({ friend, onSplitBill }) {
+  const [billValue, setBillValue] = useState("");
+  const [myExpense, setMyExpense] = useState("");
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
+  const paidByFriend = billValue ? billValue - myExpense : "";
+
+  function handleSubit(e) {
+    e.preventDefault();
+
+    if (!billValue || myExpense) return;
+
+    onSplitBill(whoIsPaying === "user" ? paidByFriend : -myExpense);
+  }
+  return (
+    <form className="form-split-bill" onSubmit={handleSubit}>
+      <h2>Split the bill with {friend.name}</h2>
+
+      <label>💸 Bill value</label>
+      <input
+        type="text"
+        value={billValue}
+        onChange={(e) => setBillValue(Number(e.target.value))}
+        placeholder="Bill amount"
+      />
+
+      <label>🧍‍♂️ Your expense</label>
+      <input
+        type="text"
+        value={myExpense}
+        onChange={(e) =>
+          setMyExpense(
+            Number(e.target.value) > billValue
+              ? billValue
+              : Number(e.target.value)
+          )
+        }
+        placeholder="My expense"
+      />
+
+      <label>👫 {friend.name}'s expense</label>
+      <input type="number" value={paidByFriend} disabled />
+
+      <label>💰 Who is paying the bill</label>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
+        <option value="user">Me</option>
+        <option value={friend.name}>{friend.name}</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  );
+}
